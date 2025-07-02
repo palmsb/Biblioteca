c6cdb0c99e98476b4343631f43992679ed4b0f9b
Larissa Maiara <94052203+Larissa-maiara@users.noreply.github.com>
2025-07-02 20:38:40 -0300

    Update livro.java

--- src/modelos/livro.java
+++ src/modelos/livro.java
@@ -1,9 +1,24 @@
 public class Livro {
     private String codigo;
     private String titulo;
+    private String editora;
+    private List<String> autores;
+    private String edicao;
+    private int anoPublicacao;
+    
     private List<Exemplar> exemplares;
     private List<Reserva> reservas;
     private List<ObservadorDeReserva> observadores;
+
+    public Livro(int codigo, String titulo, String editora, List<String> autores, String edicao, int anoPublicacao) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.editora = editora;
+        this.autores = autores;
+        this.edicao = edicao;
+        this.anoPublicacao = anoPublicacao;
+    }
+
 }
 
 //criar a função para notificar
