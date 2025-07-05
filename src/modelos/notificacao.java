d44850a43920e11bb0faffe71cc12b65625b3906
Larissa Maiara <94052203+Larissa-maiara@users.noreply.github.com>
2025-07-05 16:37:13 -0300

    Update notificacao.java

--- src/modelos/notificacao.java
+++ src/modelos/notificacao.java
@@ -1,5 +1,33 @@
+package modelos;
+
+import java.time.LocalDate;
+
 public class Notificacao {
     private Professor observador;
     private Livro livro;
     private LocalDate data;
+
+    public Notificacao(Professor observador, Livro livro, LocalDate data) {
+        this.observador = observador;
+        this.livro = livro;
+        this.data = data;
+    }
+
+    public Professor getObservador() {
+        return observador;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Notificação para %s sobre o livro \"%s\" em %s",
+                observador.getNome(), livro.getTitulo(), data);
+    }
 }
