eae32eef1710a82e6201dde7f56c97c42d427698
Larissa Maiara <94052203+Larissa-maiara@users.noreply.github.com>
2025-07-05 16:22:08 -0300

    Update exemplar.java

--- src/modelos/exemplar.java
+++ src/modelos/exemplar.java
@@ -1,4 +1,36 @@
+package modelos;
+
 public class Exemplar {
     private String codigo;
     private boolean emprestado;
+
+    public Exemplar(String codigo) {
+        this.codigo = codigo;
+        this.emprestado = false; // por padrão, está disponível
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public boolean isEmprestado() {
+        return emprestado;
+    }
+
+    public boolean isDisponivel() {
+        return !emprestado;
+    }
+
+    public void emprestar() {
+        this.emprestado = true;
+    }
+
+    public void devolver() {
+        this.emprestado = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Exemplar %s - %s", codigo, emprestado ? "Emprestado" : "Disponível");
+    }
 }
