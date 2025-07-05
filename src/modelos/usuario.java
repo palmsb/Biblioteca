815a4d4695cff8dfebcf3e8d6df8075154a28293
Larissa Maiara <94052203+Larissa-maiara@users.noreply.github.com>
2025-07-05 16:34:16 -0300

    Update usuario.java

--- src/modelos/usuario.java
+++ src/modelos/usuario.java
@@ -1,8 +1,45 @@
+package modelos;
+
+import estrategias.EstrategiaEmprestimo;
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Usuario {
     protected String codigo;
     protected String nome;
     protected EstrategiaEmprestimo estrategia;
 
-    public abstract String getTipo();
-}
+    protected List<Emprestimo> emprestimos;
+    protected List<Reserva> reservas;
 
+    public Usuario(String codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.emprestimos = new ArrayList<>();
+        this.reservas = new ArrayList<>();
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public EstrategiaEmprestimo getEstrategia() {
+        return estrategia;
+    }
+
+    public List<Emprestimo> getEmprestimos() {
+        return emprestimos;
+    }
+
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
+
+    public abstract String getTipo();
+    public abstract int getPrazoEmprestimoEmDias();
+    public abstract int getLimiteEmprestimos();
+}
