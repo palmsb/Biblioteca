00b68f5e889ce45bffc8f4e9fe4f5e2c1e160a70
Larissa Maiara <94052203+Larissa-maiara@users.noreply.github.com>
2025-07-05 16:24:39 -0300

    Update reserva.java

--- src/modelos/reserva.java
+++ src/modelos/reserva.java
@@ -1,4 +1,26 @@
+package modelos;
+
+import java.time.LocalDate;
+
 public class Reserva {
     private Usuario usuario;
     private LocalDate dataReserva;
+
+    public Reserva(Usuario usuario, LocalDate dataReserva) {
+        this.usuario = usuario;
+        this.dataReserva = dataReserva;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public LocalDate getDataReserva() {
+        return dataReserva;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Reserva de %s em %s", usuario.getNome(), dataReserva);
+    }
 }
